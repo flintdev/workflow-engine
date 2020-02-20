da572783bfc7a012e2011565305c4259ce3dd1d4
Gaoxin Dai <daigx1990@gmmail.com>
2020-02-20 14:19:31 -0800

    update trigger.go

--- workflow-example/workflows/workflow1/trigger.go
+++ workflow-example/workflows/workflow1/trigger.go
@@ -1,1 +1,9 @@
 package workflow1
+
+import (
+	workflowFramework "workflow-engine/engine"
+)
+
+func TriggerCondition(event workflowFramework.Event) bool {
+	return event.Model == "expense" && event.Type == "ADD"
+}
