babffa5163439015de05ecf47bbaf982f7699089
Gaoxin Dai <daigx1990@gmmail.com>
2020-03-13 17:02:39 -0700

    integrate python executor

--- workflow-example/workflows/workflow1/trigger.go
+++
@@ -1,9 +0,0 @@
-package workflow1
-
-import (
-	workflowFramework "github.com/flintdev/workflow-engine/engine"
-)
-
-func TriggerCondition(event workflowFramework.Event) bool {
-	return event.Model == "expense" && event.Type == "ADDED"
-}
--- workflow-example/workflows/workflow2/trigger.go
+++
@@ -1,9 +0,0 @@
-package workflow2
-
-import (
-	workflowFramework "github.com/flintdev/workflow-engine/engine"
-)
-
-func TriggerCondition(event workflowFramework.Event) bool {
-	return event.Model == "approval" && event.Type == "ADDED"
-}
